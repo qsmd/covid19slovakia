435ddbe704ce179febc096b70565499fad190a6b
Martin Adamek <martin.adamek@gmail.com>
2020-03-20 15:41:06 +0100

    Adding templates for UK and US

---
+++ data/uk.js
@@ -0,0 +1,8 @@
+OTHER_COUNTRIES.push({
+    name: 'UK',
+    population: 67545757,
+    color: window.chartColors.green,
+    data: [
+      ['19.3.2020', 2630, ],
+    ]
+  });
---
+++ data/us.js
@@ -0,0 +1,8 @@
+OTHER_COUNTRIES.push({
+    name: 'US',
+    population: 328239523,
+    color: window.chartColors.purple,
+    data: [
+        ['19.3.2020', 7087, ],
+    ]
+  });
