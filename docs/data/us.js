83484c542aaa66e8c2c4c6d790c0b842f2ed9133
Martin Adamek <martin.adamek@gmail.com>
2020-03-23 16:02:56 +0100

    Alternative in docs folder to server from there instead of gh-pages branch

---
+++ docs/data/us.js
@@ -0,0 +1,11 @@
+COUNTRIES.push({ // eslint-disable-line no-undef
+  name: 'US',
+  population: 328239523,
+  color: '#cccccc',
+  data: [
+    ['19.3.2020', 7087, ],
+    ['20.3.2020', 10442, ],
+    ['21.3.2020', 15219, ],
+    ['22.3.2020', 15219, ],
+  ],
+});
