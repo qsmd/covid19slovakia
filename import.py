9c7605e2e5e612d61926c99e8feb2c46188c7182
Martin Adamek <martin.adamek@gmail.com>
2020-03-24 15:19:08 +0100

    First reasonable parsing to JS format

--- import.py
+++ import.py
@@ -1,20 +1,35 @@
 import csv
 
-watched_countries = ['Slovakia', 'Czechia', 'United Kingdom']
+INDEX_FIRST_DAY = 4
+
+watched_countries = {'Slovakia', 'Czechia', 'United Kingdom'}
+
+def get_nonzero_idx(days):
+    for index, day in enumerate(days[INDEX_FIRST_DAY:]):
+        if day != '0':
+            return INDEX_FIRST_DAY + index
 
 def get_days(days):
-    row = ''
-    for day in days:
-        if day != '0':
-            row += f'{day},'
-    return row
+    nonzero_idx = get_nonzero_idx(days)
+    nonzero_days = []
+    for day in days[nonzero_idx:]:
+        nonzero_days.append(day)
+    return nonzero_idx, nonzero_days
+
+def date_sk(date):
+    a = date.split('/')
+    return f'{a[1]}.{a[0]}.{a[2]}'
 
 def process_file():
     with open('time_series_covid19_confirmed_global.csv') as f:
-        data = csv.reader(f)
-        for row in data:
+        header = None
+        rows = csv.reader(f)
+        for row in rows:
+            if header is None:
+                header = row
             if row[0] == '' and row[1] in watched_countries:
-                print(f"'{row[1]}': [{get_days(row[4:])}],")
+                idx, days = get_days(row)
+                print(f"['{row[1]}','{date_sk(header[idx])}',{','.join(days)}],")
 
 print('COUNTRIES = [')
 process_file()
