4b10513ef7569befc6812be3cdd74f3db4c6ca01
Martin Adamek <martin.adamek@gmail.com>
2020-03-25 20:48:41 +0100

    Reorder SK and CZ to the beginning

--- import.py
+++ import.py
@@ -35,13 +35,26 @@ def date_sk(date):
 def process_file():
     with open('time_series_covid19_confirmed_global.csv') as f:
         header = None
+        js_line_sk = ''
+        js_line_cz = ''
+        js_lines = []
         rows = csv.reader(f)
         for row in rows:
             if header is None:
                 header = row
             if row[0] == '' and row[1] in COUNTRIES:
                 idx, days = get_days(row)
-                print(f"  ['{COUNTRIES[row[1]]}','{row[1]}','{date_sk(header[idx])}',{','.join(days)}],")
+                js_line = f"  ['{COUNTRIES[row[1]]}','{row[1]}','{date_sk(header[idx])}',{','.join(days)}],"
+                if row[1] == 'Slovakia':
+                    js_line_sk = js_line
+                elif row[1] == 'Czechia':
+                    js_line_cz = js_line
+                else:
+                    js_lines.append(js_line)
+        js_lines.insert(0, js_line_cz)
+        js_lines.insert(0, js_line_sk)
+        for line in js_lines:
+            print(line)
 
 print('/* eslint-disable comma-spacing */')
 print('const COUNTRIES = [')
