158a02695940283144211005483b5409f1d1c581
Martin Adamek <martin.adamek@gmail.com>
2021-03-31 09:48:17 +0200

    Data update

--- docs/data.js
+++ docs/data.js
@@ -8,7 +8,9 @@ import * as chartjs from './Chart.bundle.min.js';
 import ChartConfig from './ChartConfig.js';
 
 window.onload = function () {
+	console.log("### 111")
   ['total-cases', 'daily-cases', 'total-active', 'daily-active'].forEach((canvasId) => {
+		console.log("### 222 " + canvasId)
     const chartConfig = new ChartConfig(canvasId);
     const chart = new Chart(document.getElementById(`canvas-${canvasId}`).getContext('2d'), chartConfig.createConfig());
     document.getElementById(`config-${canvasId}`).addEventListener('click', () => { chartConfig.collapseClick(); });
